dff421dd037851084e683ad099941d10ab7f5270
João Rodolpho <joaorodolphocabral@gmail.com>
2024-04-25 16:08:54 -0300

    versão mais avançada

--- br/com/sistemaBancario/__init__.py
+++ br/com/sistemaBancario/__init__.py
@@ -1,9 +1,51 @@
+
+
+LIMITE_VALOR_SAQUE = 500
+LIMITE_DIARIO_SAQUE = 3
+
+MENU = """
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+
+saldo = 0.0
+extrato_deposito = []
+extrato_saque = []
+
+while True:
+
+    opcao = input(MENU)
+
+    if opcao == "d":
+        depositar(5000)
+
+    elif opcao == "s":
+
+    elif opcao == "e":
+
+    elif opcao == "q":
+        break
+
 def depositar(valor):
+    global saldo
 
-    pass
+    if valor > 0:
+        saldo += valor
 
-def sacar():
+
+def sacar(valor):
     pass
 
 def extrato():
-    pass
+    global extrato_deposito, extrato_saque
+
+    for deposito in extrato_deposito:
+        print(f"R$ {deposito}")
+
+    for saque in extrato_saque:
+        print(f"R$ {saque}")
+
