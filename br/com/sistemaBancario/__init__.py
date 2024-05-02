42d4f54686c18c618f165e519adcf744e9adc336
João Rodolpho <joaorodolphocabral@gmail.com>
2024-05-01 21:33:02 -0300

    implementação de todas os pontos da versão 1 em forma de função

--- br/com/sistemaBancario/__init__.py
+++ br/com/sistemaBancario/__init__.py
@@ -1,80 +1,95 @@
-def depositar(saldo_depositar):
-    valor = float(input("Insira um valor para depositar: "))
-    flag = valida_depositar(valor_depositar=valor)
+def menu():
+    opcoes = """
+
+    [d] Depositar
+    [s] Sacar
+    [e] Extrato
+    [q] Sair
+
+    => """
+
+    saldo = 0.0
+    extrato_deposito = ""
+    extrato_saque = ""
+    numero_saques = 0
+
+    while True:
+
+        opcao = input(opcoes)
+
+        if opcao == "d":
+            valor = float(input("Insira um valor para depositar: "))
+            saldo, extrato_deposito = depositar(depositar_saldo=saldo, depositar_valor=valor,
+                                                depositar_extrato=extrato_deposito)
+
+        elif opcao == "s":
+            valor = float(input("Insira um valor para sacar: "))
+            saldo, extrato_saque, numero_saques = sacar(sacar_saldo=saldo, sacar_extrato=extrato_saque, sacar_valor=valor,
+                                         sacar_numeros_saques=numero_saques)
+
+        elif opcao == "e":
+            exibir_extrato(exibir_extrato_deposito=extrato_deposito, exibir_extrato_saque=extrato_saque)
+
+        elif opcao == "q":
+            break
+
+        else:
+            print("Operação falhou! Opção invalida")
+
+
+def depositar(depositar_saldo, depositar_valor, depositar_extrato):
+    flag = valida_depositar(valida_depositar_valor= depositar_valor)
     if flag:
-        saldo_depositar += valor
-        return saldo_depositar
-    else:
-        pass # retornar algo para caso valor seja invalido
+        depositar_saldo += depositar_valor
+        depositar_extrato += f"Depósito: R$ {depositar_valor:,.2f}\n"
+    return [depositar_saldo, depositar_extrato]
 
 
-def valida_depositar(valor_depositar):
-    if valor_depositar <= 0:
+def valida_depositar(valida_depositar_valor):
+    if valida_depositar_valor <= 0:
+        print("Operação falhou! Valor menor que 0")
         return False
     else:
         return True
 
 
-def sacar(*, saldo_sacar, extrato_sacar, numeros_saques):
-    valor = float(input("Insira um valor para depositar: "))
-    flag = valida_sacar(numeros_saques=numeros_saques, valor_valida_sacar=valor, v_saldo=saldo_sacar)
+def sacar(*, sacar_saldo, sacar_valor, sacar_extrato, sacar_numeros_saques):
+
+    flag = valida_sacar(valida_sacar_numeros_saques=sacar_numeros_saques, valida_sacar_valor=sacar_valor,
+                        valida_sacar_saldo=sacar_saldo)
+    if flag:
+        sacar_saldo -= sacar_valor
+        sacar_extrato += f"Saque: R$ {sacar_valor:.2f}\n"
+        sacar_numeros_saques += 1
+    return [sacar_saldo, sacar_extrato, sacar_numeros_saques]
 
 
-def valida_sacar(*, numeros_saques, valor_valida_sacar, v_saldo):
-    LIMITE_VALOR_SAQUE = 500
-    LIMITE_DIARIO_SAQUE = 3
+def valida_sacar(*, valida_sacar_numeros_saques, valida_sacar_valor, valida_sacar_saldo):
+    limite_valor_saque = 500
+    limite_diario_saque = 3
 
-    if numeros_saques == LIMITE_DIARIO_SAQUE:
+    if valida_sacar_numeros_saques == limite_diario_saque:
+        print("Operação falhou! Limite diário atingido")
         return False
-    elif valor_valida_sacar <= 0 or valor_valida_sacar > LIMITE_VALOR_SAQUE:
+
+    elif valida_sacar_valor <= 0 or valida_sacar_valor > limite_valor_saque:
+        print("Operação falhou! Valor menor que 0 ou maior que o limite")
         return False
-    elif v_saldo < valor_valida_sacar:
+
+    elif valida_sacar_saldo < valida_sacar_valor:
+        print("Operação falhou! Saldo insuficiente")
         return False
+
     else:
         return True
 
 
-def exibir_extrato(ext_deposito, ext_saque):
-    for deposito in ext_deposito:
-        print(f"R$ {deposito}")
-
-    for saque in ext_saque:
-        print(f"R$ {saque}")
+def exibir_extrato(exibir_extrato_deposito, exibir_extrato_saque):
+    print(exibir_extrato_deposito)
+    print(exibir_extrato_saque)
 
 
-MENU = """
-
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
-
-=> """
-
-saldo = 0.0
-extrato_deposito = []
-extrato_saque = []
-
-while True:
-
-    opcao = input(MENU)
-
-    if opcao == "d":
-        pass
-
-    elif opcao == "s":
-        pass
-        # sacar(wa)
-
-    elif opcao == "e":
-        pass
-
-    elif opcao == "q":
-        break
-
-    else:
-        print("Operação falhou! Opção invalida")
-
+menu()
 # criar usuario
 # validar cadastro - cpfs diferentes
 # armazera usuario em lista
